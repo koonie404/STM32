f594ed5c889ca9ecd5f139e2c58973d94524a7ee
Cheong-Su Lim <flcldnd@gmail.com>
2026-01-21 16:29:32 +0900

    Add files via upload

--- NUCLEO_F411RE/08.CLCD/08_02.ADC_Temperature Sensor_CLCD/main.c
+++ NUCLEO_F411RE/08.CLCD/08_02.ADC_Temperature Sensor_CLCD/main.c
@@ -4,16 +4,6 @@
   * @file           : main.c
   * @brief          : Main program body
   ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2026 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
   */
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
@@ -32,7 +22,6 @@
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
 #define delay_ms HAL_Delay
-
 #define ADDRESS   0x27 << 1
 
 #define RS1_EN1   0x05
@@ -60,7 +49,7 @@ const float ADC_TO_VOLT = 3.3 / 4096.0;
 uint16_t adc1;
 float vSense;
 float temp;
-char lcd_buf[16];
+char lcd_buf[17];
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -72,7 +61,6 @@ static void MX_I2C1_Init(void);
 
 /* USER CODE BEGIN PFP */
 void I2C_ScanAddresses(void);
-
 void delay_us(int us);
 void LCD_DATA(uint8_t data);
 void LCD_CMD(uint8_t cmd);
@@ -86,8 +74,6 @@ void LCD_PUTS(char *str);
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
 #ifdef __GNUC__
-/* With GCC/small printf (option LD Linker -> Libraries -> small printf,
-   set to 'Yes') calls __io_putchar() */
 int __io_putchar(int ch)
 #else
 int fputc(int ch, FILE *f)
@@ -102,16 +88,13 @@ int fputc(int ch, FILE *f)
 void I2C_ScanAddresses(void) {
     HAL_StatusTypeDef result;
     uint8_t i;
-
     printf("Scanning I2C addresses...\r\n");
-
     for (i = 1; i < 128; i++) {
         result = HAL_I2C_IsDeviceReady(&hi2c1, (uint16_t)(i << 1), 1, 10);
         if (result == HAL_OK) {
             printf("I2C device found at address 0x%02X\r\n", i);
         }
     }
-
     printf("Scan complete.\r\n");
 }
 
@@ -126,7 +109,6 @@ void LCD_DATA(uint8_t data) {
     HAL_I2C_Master_Transmit(&hi2c1, ADDRESS, &temp, 1, 100);
     temp = (data & 0xF0) | RS1_EN0 | BackLight;
     HAL_I2C_Master_Transmit(&hi2c1, ADDRESS, &temp, 1, 100);
-
     temp = ((data << 4) & 0xF0) | RS1_EN1 | BackLight;
     HAL_I2C_Master_Transmit(&hi2c1, ADDRESS, &temp, 1, 100);
     temp = ((data << 4) & 0xF0) | RS1_EN0 | BackLight;
@@ -140,7 +122,6 @@ void LCD_CMD(uint8_t cmd) {
     HAL_I2C_Master_Transmit(&hi2c1, ADDRESS, &temp, 1, 100);
     temp = (cmd & 0xF0) | RS0_EN0 | BackLight;
     HAL_I2C_Master_Transmit(&hi2c1, ADDRESS, &temp, 1, 100);
-
     temp = ((cmd << 4) & 0xF0) | RS0_EN1 | BackLight;
     HAL_I2C_Master_Transmit(&hi2c1, ADDRESS, &temp, 1, 100);
     temp = ((cmd << 4) & 0xF0) | RS0_EN0 | BackLight;
@@ -157,7 +138,6 @@ void LCD_CMD_4bit(uint8_t cmd) {
     delay_us(50);
 }
 
-
 void LCD_INIT(void) {
     delay_ms(100);
     LCD_CMD_4bit(0x03); delay_ms(5);
@@ -169,7 +149,6 @@ void LCD_INIT(void) {
     LCD_CMD(0x01);
     delay_ms(2);
 }
-
 
 void LCD_XY(char x, char y) {
     if (y == 0) LCD_CMD(0x80 + x);
@@ -183,6 +162,41 @@ void LCD_CLEAR(void) {
 
 void LCD_PUTS(char *str) {
     while (*str) LCD_DATA(*str++);
+}
+
+// 온도계 커스텀 문자 (5x8 픽셀)
+const uint8_t thermometer[8] = {
+    0b00100,
+    0b01010,
+    0b01010,
+    0b01010,
+    0b01110,
+    0b11111,
+    0b11111,
+    0b01110
+};
+
+/**
+ * @brief CGRAM에 커스텀 문자 등록
+ * @param location: 문자 번호 (0~7)
+ * @param pattern: 8바이트 패턴 배열
+ */
+void LCD_CreateChar(uint8_t location, const uint8_t *pattern) {
+    if (location > 7) return;
+    LCD_CMD(0x40 | (location << 3));
+    for (int i = 0; i < 8; i++) {
+        LCD_DATA(pattern[i]);
+    }
+    LCD_CMD(0x80);
+}
+
+/**
+ * @brief 커스텀 문자 출력
+ * @param location: 등록된 문자 번호 (0~7)
+ */
+void LCD_PutCustomChar(uint8_t location) {
+    if (location > 7) return;
+    LCD_DATA(location);
 }
 /* USER CODE END 0 */
 
@@ -222,9 +236,14 @@ int main(void)
   I2C_ScanAddresses();
 
   LCD_INIT();
-  LCD_XY(0, 0); LCD_PUTS((char *)"Temperature");
-  LCD_XY(0, 1); LCD_PUTS((char *)"Monitoring...");
-  HAL_Delay(1000);
+
+  // 온도계 커스텀 문자 등록 (위치 0번에 저장)
+  LCD_CreateChar(0, thermometer);
+
+  LCD_XY(0, 0); LCD_PUTS((char *)"STM32 Temp");
+  LCD_XY(0, 1); LCD_PUTS((char *)"Monitor v1.0");
+  HAL_Delay(2000);
+  LCD_CLEAR();
 
   /* Start calibration*/
   if (HAL_ADCEx_Calibration_Start(&hadc1) != HAL_OK)
@@ -243,23 +262,27 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
-	    HAL_ADC_PollForConversion(&hadc1, 100);
-	    adc1 = HAL_ADC_GetValue(&hadc1);
+    HAL_ADC_PollForConversion(&hadc1, 100);
+    adc1 = HAL_ADC_GetValue(&hadc1);
 
-	    vSense = adc1 * ADC_TO_VOLT;
-	    temp = (V25 - vSense) / AVG_SLOPE + 25.0;
+    vSense = adc1 * ADC_TO_VOLT;
+    temp = (V25 - vSense) / AVG_SLOPE + 25.0;
 
-	    snprintf(lcd_buf, sizeof(lcd_buf),
-	             "CPU ADC : %4d", adc1);
-	    LCD_XY(0, 0);
-	    LCD_PUTS(lcd_buf);
+    // 첫 번째 줄: [온도계] CPU ADC : 0000
+    LCD_XY(0, 0);
+    LCD_PutCustomChar(0);  // 온도계 아이콘
+    snprintf(lcd_buf, sizeof(lcd_buf), "CPU ADC : %4d", adc1);
+    LCD_PUTS(lcd_buf);
 
-	    snprintf(lcd_buf, sizeof(lcd_buf),
-	             "CPU Temp: %2.0f%cC", temp, 0xDF);
-	    LCD_XY(0, 1);
-	    LCD_PUTS(lcd_buf);
+    // 두 번째 줄: [온도계] CPU Temp: 00°C
+    LCD_XY(0, 1);
+    LCD_PutCustomChar(0);  // 온도계 아이콘
+    snprintf(lcd_buf, sizeof(lcd_buf), "CPU Temp:%3.0f%cC", temp, 0xDF);
+    LCD_PUTS(lcd_buf);
 
-	    HAL_Delay(500);
+    printf("ADC:%d  Temp:%.2fC\r\n", adc1, temp);
+
+    HAL_Delay(500);
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
@@ -277,9 +300,6 @@ void SystemClock_Config(void)
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
   RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
 
-  /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
   RCC_OscInitStruct.HSIState = RCC_HSI_ON;
   RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
@@ -291,8 +311,6 @@ void SystemClock_Config(void)
     Error_Handler();
   }
 
-  /** Initializes the CPU, AHB and APB buses clocks
-  */
   RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
                               |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
@@ -319,19 +337,8 @@ void SystemClock_Config(void)
   */
 static void MX_ADC1_Init(void)
 {
-
-  /* USER CODE BEGIN ADC1_Init 0 */
-
-  /* USER CODE END ADC1_Init 0 */
-
   ADC_ChannelConfTypeDef sConfig = {0};
 
-  /* USER CODE BEGIN ADC1_Init 1 */
-
-  /* USER CODE END ADC1_Init 1 */
-
-  /** Common config
-  */
   hadc1.Instance = ADC1;
   hadc1.Init.ScanConvMode = ADC_SCAN_DISABLE;
   hadc1.Init.ContinuousConvMode = ENABLE;
@@ -344,19 +351,13 @@ static void MX_ADC1_Init(void)
     Error_Handler();
   }
 
-  /** Configure Regular Channel
-  */
   sConfig.Channel = ADC_CHANNEL_TEMPSENSOR;
   sConfig.Rank = ADC_REGULAR_RANK_1;
-  sConfig.SamplingTime = ADC_SAMPLETIME_239CYCLES_5;  // 샘플링 시간 증가
+  sConfig.SamplingTime = ADC_SAMPLETIME_239CYCLES_5;
   if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
   {
     Error_Handler();
   }
-  /* USER CODE BEGIN ADC1_Init 2 */
-
-  /* USER CODE END ADC1_Init 2 */
-
 }
 
 /**
@@ -366,30 +367,19 @@ static void MX_ADC1_Init(void)
   */
 static void MX_I2C1_Init(void)
 {
-  /* USER CODE BEGIN I2C1_Init 0 */
-
-  /* USER CODE END I2C1_Init 0 */
-
-  /* USER CODE BEGIN I2C1_Init 1 */
-
-  /* USER CODE END I2C1_Init 1 */
   hi2c1.Instance = I2C1;
   hi2c1.Init.ClockSpeed = 100000;
-  hi2c1.Init.DutyCycle = 0x00000000U;  // I2C_DUTYCYCLE_2 대체
+  hi2c1.Init.DutyCycle = I2C_DUTYCYCLE_2;
   hi2c1.Init.OwnAddress1 = 0;
-  hi2c1.Init.AddressingMode = 0x00004000U;  // I2C_ADDRESSINGMODE_7BIT 대체
-  hi2c1.Init.DualAddressMode = 0x00000000U;  // I2C_DUALADDRESS_DISABLE 대체
+  hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+  hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
   hi2c1.Init.OwnAddress2 = 0;
-  hi2c1.Init.GeneralCallMode = 0x00000000U;  // I2C_GENERALCALL_DISABLE 대체
-  hi2c1.Init.NoStretchMode = 0x00000000U;  // I2C_NOSTRETCH_DISABLE 대체
+  hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+  hi2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
   if (HAL_I2C_Init(&hi2c1) != HAL_OK)
   {
     Error_Handler();
   }
-  /* USER CODE BEGIN I2C1_Init 2 */
-
-  /* USER CODE END I2C1_Init 2 */
-
 }
 
 /**
@@ -399,14 +389,6 @@ static void MX_I2C1_Init(void)
   */
 static void MX_USART2_UART_Init(void)
 {
-
-  /* USER CODE BEGIN USART2_Init 0 */
-
-  /* USER CODE END USART2_Init 0 */
-
-  /* USER CODE BEGIN USART2_Init 1 */
-
-  /* USER CODE END USART2_Init 1 */
   huart2.Instance = USART2;
   huart2.Init.BaudRate = 115200;
   huart2.Init.WordLength = UART_WORDLENGTH_8B;
@@ -419,10 +401,6 @@ static void MX_USART2_UART_Init(void)
   {
     Error_Handler();
   }
-  /* USER CODE BEGIN USART2_Init 2 */
-
-  /* USER CODE END USART2_Init 2 */
-
 }
 
 /**
@@ -433,39 +411,27 @@ static void MX_USART2_UART_Init(void)
 static void MX_GPIO_Init(void)
 {
   GPIO_InitTypeDef GPIO_InitStruct = {0};
-  /* USER CODE BEGIN MX_GPIO_Init_1 */
 
-  /* USER CODE END MX_GPIO_Init_1 */
-
-  /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOC_CLK_ENABLE();
   __HAL_RCC_GPIOD_CLK_ENABLE();
   __HAL_RCC_GPIOA_CLK_ENABLE();
   __HAL_RCC_GPIOB_CLK_ENABLE();
 
-  /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_RESET);
 
-  /*Configure GPIO pin : B1_Pin */
   GPIO_InitStruct.Pin = B1_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(B1_GPIO_Port, &GPIO_InitStruct);
 
-  /*Configure GPIO pin : LD2_Pin */
   GPIO_InitStruct.Pin = LD2_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(LD2_GPIO_Port, &GPIO_InitStruct);
 
-  /* EXTI interrupt init*/
   HAL_NVIC_SetPriority(EXTI15_10_IRQn, 0, 0);
   HAL_NVIC_EnableIRQ(EXTI15_10_IRQn);
-
-  /* USER CODE BEGIN MX_GPIO_Init_2 */
-
-  /* USER CODE END MX_GPIO_Init_2 */
 }
 
 /* USER CODE BEGIN 4 */
@@ -478,28 +444,14 @@ static void MX_GPIO_Init(void)
   */
 void Error_Handler(void)
 {
-  /* USER CODE BEGIN Error_Handler_Debug */
-  /* User can add his own implementation to report the HAL error return state */
   __disable_irq();
   while (1)
   {
   }
-  /* USER CODE END Error_Handler_Debug */
 }
 
 #ifdef  USE_FULL_ASSERT
-/**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
 void assert_failed(uint8_t *file, uint32_t line)
 {
-  /* USER CODE BEGIN 6 */
-  /* User can add his own implementation to report the file name and line number,
-     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-  /* USER CODE END 6 */
 }
-#endif /* USE_FULL_ASSERT */
+#endif
